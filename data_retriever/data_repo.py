2e4f4b28c5a4d0c6167a227b0a832c15391e9dc6
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-07-11 21:14:06 -0700

    Create data_repo.py

---
+++ data_retriever/data_repo.py
@@ -0,0 +1,12 @@
+
+class DataRepo:
+  
+    def __init__(self, llm):
+        self.llm = llm
+        self.annotated_gdfs = []
+        self.annotated_dfs = []
+
+    def contains_gdf(self, nl_query):
+        pass
+
+    
